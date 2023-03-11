17ca083a4bc9d7056ba2de3a53c654bac2edfd9e
Abhishekkumar969 <dubeyji9631@gmail.com>
2023-03-11 18:46:29 +0530

    new

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const heroSection = document.querySelector(".section-hero");
+
+// ===========================================
+//    creating a responsive navbar component 
+// ===========================================
+
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const headerElem = document.querySelector(".header");
+
+mobile_nav.addEventListener("click", () => {
+    headerElem.classList.toggle("active");
+})
+
 // ===========================================
 //    creating a portfolio tabbed component 
 // ===========================================
@@ -30,17 +43,156 @@ p_btns.addEventListener("click", (e) => {
 
 
 
-// swiper js code 
+//----------------------------- swiper js code ---------------------------------------------
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+
+new Swiper(".mySwiper", {
+    slidesPerView: 2,
     spaceBetween: 30,
     autoplay: {
         delay: 2500,
-        // disableOnInteraction: false,
+        disableOnInteraction: false,
     },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
 });
+
+const myJsmedia = (widthSize) => {
+    if (widthSize.matches) {
+        new Swiper(".mySwiper", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        });
+    } else {
+        new Swiper(".mySwiper", {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        });
+    }
+};
+
+const widthSize = window.matchMedia("(max-width:780px)");
+// call listener fn at run time 
+myJsmedia(widthSize);
+// attach listener fn on state changes 
+widthSize.addEventListener("change", myJsmedia);
+
+
+//------------------------- scroll to top button--------------------------------------------
+
+
+const footerElem = document.querySelector(".section-footer");
+
+const scrollElement = document.createElement("div");
+scrollElement.classList.add("scrollTop-style");
+
+scrollElement.innerHTML = `<ion-icon name="arrow-up-outline" class="scroll-top"></ion-icon>`;
+
+footerElem.after(scrollElement);
+
+const scrollTop = () => {
+    heroSection.scrollIntoView({ behavior: "smooth" });
+};
+
+scrollElement.addEventListener("click", scrollTop);
+
+//-------------------------smooth scroll effect----------------------------------------------
+
+const portfolioSec = document.querySelector(".section-portfolio");
+const contactSec = document.querySelector(".section-contact");
+
+document.querySelector(".portfolio-link").addEventListener("click", () => {
+    portfolioSec.scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelector(".hireme-btn").addEventListener("click", (e) => {
+    e.preventDefault();
+    contactSec.scrollIntoView({ behavior: "smooth" });
+});
+//--------------------- creating a sticky navbar component ----------------------------------
+
+
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        const ent = entries[0];
+        // console.log(ent);
+        !ent.isIntersecting ?
+            document.body.classList.add("sticky") :
+            document.body.classList.remove("sticky");
+
+    }, {
+        root: null,
+        threshold: 0,
+    });
+
+
+observer.observe(heroSection);
+
+
+
+const workSection = document.querySelector(".section-work-data");
+const workObserver = new IntersectionObserver((entries, observe) => {
+    const [entry] = entries;
+    // console.log(entry);
+
+    // if (entry.isIntersecting == false)
+    if (!entry.isIntersecting) return;
+
+    //----------------------- animate number counter---------------------------------------------
+
+    const counterNum = document.querySelectorAll(".counter-numbers");
+
+    const speed = 200;
+
+    counterNum.forEach((curElem) => {
+        const updateNumber = () => {
+            const targetNumber = parseInt(curElem.dataset.number);
+            // console.log(targetNumber);
+            const initialNum = parseInt(curElem.innerText);
+            // console.log(initialNum);
+
+            const incrementNumber = Math.trunc(targetNumber / speed);
+            // console.log(incrementNumber);
+
+            if (initialNum < targetNumber) {
+                curElem.innerText = `${initialNum + incrementNumber}+`;
+                setTimeout(updateNumber, 10);
+            }
+        };
+
+        updateNumber();
+    });
+    observe.unobserve(workSection);
+
+}, {
+    root: null,
+    threshold: 0,
+});
+
+workObserver.observe(workSection);
+
+// document.body.classList.add("sticky");
+// // if (entry.isIntersecting == false)
+// if (!ent.isIntersecting) return;
+// observer.unobserve(workSection);
+
+
+// lazy loading images 
+
+const imgRef = document.querySelector("img[data-src]");
+
+const lazyImg = (entries) => {
+    const [entry] = entries;
+    console.log(entry);
+    if (!entry.isIntersecting) return;
+    entry.target.src = imgRef.dataset.src;
+};
+const imgObserver = new IntersectionObserver(lazyImg, {
+    root: null,
+    threshold: 0,
+})
+
+imgObserver.observe(imgRef);
